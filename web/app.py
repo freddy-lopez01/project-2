1b96d07c6b369df1c6551c6582cbd6b670c59731
freddy lopez <freddylopez@dyn-10-108-66-213.wless.uoregon.edu>
2023-01-30 18:19:51 -0800

    Project2 consists of implementing a file-checking system that will check if a user requested file exists and/or said requested file contains illegal charcters. The program reads specified PORT and DEBUG values from credentials.ini (which is not pushed and on canvas) and uses them when running the Flask app image. This was my first interaction using Flask and learning about DOCKER. The Dockerfile is also updated to have personal name and email as required per the instructions

--- web/app.py
+++ web/app.py
@@ -2,13 +2,62 @@
 John Doe's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, send_from_directory, render_template, request
+import os 
+from os import path
+import configparser
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+@app.route("/<path:request>")
+    
+def hello(request):
+    print(request)
+    cwd = os.getcwd()
+    print(cwd)
+    ncwd = cwd + "/pages" + "/"+request
 
+    if "~" in request or ".." in request:
+        abort(403)
+
+    elif path.exists(ncwd) == False:
+        abort(404)
+
+    elif path.exists(ncwd):
+        return send_from_directory('pages/', request), 200
+        
+    # abort(403)
+    # abort(404)
+    '''return send_from_directory("hello.html")'''
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return send_from_directory('pages/', '403.html'), 403
+
+@app.errorhandler(404)
+def file_not_found(e):
+    return send_from_directory('pages/', '404.html'), 404
+
+
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    config  = parse_config(["credentials.ini", "default.ini"])
+    port1 = config["SERVER"]["PORT"]
+    debug1 = config["SERVER"]["DEBUG"]
+    app.run(debug=debug1, host='0.0.0.0', port=port1)
+
+
